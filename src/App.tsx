68c93fd18d9a2d50ba50752638bdef2439f5492f
nisamozr <codeexplorkl10@gmail.com>
2024-12-06 14:20:26 +0530

    update generating loading mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import { IMAGES } from "./assets";
-import Sidebar from "./components/Sidebar";
-import TvPanel from "./components/TvPanel";
-import { useAppCtx } from "./context/app.contex";
-import { cn } from "./lib/utils";
+
 import router from "./routes/routes";
 
 function App() {
